4950dfca878707251b32585d9cd594aa671ccbf1
Nurgazy93 <148498987+Nurgazy93@users.noreply.github.com>
2023-11-30 22:20:15 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,43 +1,63 @@
-let inputEl1 = document.getElementById("input1");
-let inputEl2 = document.getElementById("input2");
-
-let plusBtn = document.getElementById("plus");
-let minusBtn = document.getElementById("minus");
-let multiplyBtn = document.getElementById("multiply");
-let segmentationBtn = document.getElementById("segmentation");
-
-let submitEl = document.getElementById("submit");
+/* 1. задача */
+let inputEl = document.getElementById("number1");
+let inputEl2 = document.getElementById("number2");
+let buttonEl = document.getElementById("my-button");
 let resultEl = document.getElementById("result");
-let operator;
-
-plusBtn.onclick = function () {
-    operator = "+";
-};
-minusBtn.onclick = function () {
-    operator = "-";
-};
-multiplyBtn.onclick = function () {
-    operator = "*";
-};
-segmentationBtn.onclick = function () {
-    operator = "/";
+buttonEl.onclick = function () {
+    resultEl.textContent = +inputEl.value + +inputEl2.value;
 };
 
-submitEl.onclick = function () {
-    let x = Number(inputEl1.value);
-    let y = Number(inputEl2.value);
-    let result = 0;
-    if (operator === "+") {
-        result = x + y;
+/* 2. задача */
+let inputCheck = document.getElementById("numberCheck");
+let buttonCheck = document.getElementById("buttonCheck");
+let resultCheck = document.getElementById("resultCheck");
+buttonCheck.onclick = function () {
+    if (+inputCheck.value > 0) {
+        resultCheck.textContent = "Positive";
+    } else if (+inputCheck.value == 0) {
+        resultCheck.textContent = "Zero";
+    } else {
+        resultCheck.textContent = "Negative";
     }
-    if (operator === "-") {
-        result = x - y;
+};
+
+/* 3. задача */
+let inputEvenCheck = document.getElementById("numberEvenCheck");
+let buttonEvenCheck = document.getElementById("buttonEvenCheck");
+let resultEvenCheck = document.getElementById("resultEvenCheck");
+buttonEvenCheck.onclick = function () {
+    if (+inputEvenCheck.value % 2 === 0) resultEvenCheck.textContent = "Yes";
+    else resultEvenCheck.textContent = "No";
+};
+
+/* 4. задача */
+let inputCalc1 = document.getElementById("numberCalc1");
+let inputCalc2 = document.getElementById("numberCalc2");
+let buttonCalc = document.getElementById("buttonCalc");
+let resultCalc = document.getElementById("resultCalc");
+buttonCalc.onclick = function () {
+    resultCalc.textContent = calc(+inputCalc1.value, +inputCalc2.value);
+};
+
+function calc(a, b) {
+    let plus = a + b;
+    let minus = a - b;
+    let umnojit = a * b;
+    let delit = a / b;
+    return plus + " " + minus + " " + umnojit + " " + delit;
+}
+
+/* 5. задача */
+let lenght = document.getElementById("lenght");
+let circumference = document.getElementById("circumference");
+let gender = document.getElementById("gender");
+let button = document.getElementById("getFat");
+let resultGender = document.getElementById("resultGender");
+
+button.onclick = function () {
+    if (gender.value == "male") {
+        resultGender.textContent = 64 - (20 * +lenght.value) / +circumference.value;
+    } else {
+        resultGender.textContent = 76 - (20 * +lenght.value) / +circumference.value;
     }
-    if (operator === "*") {
-        result = x * y;
-    }
-    if (operator === "/") {
-        result = x / y;
-    }
-    resultEl.textContent = result;
 };
